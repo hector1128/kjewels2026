b1623800d46d2102a23ab8631943306cd665064c
hector1128 <hectorhoraciocordero11@gmail.com>
2026-02-07 01:14:16 -0500

    added fonts, tweaked choice boxes

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
       </div>
       <div>Welcome to Kjewels</div>
       <div>UCF's Finest Jewelry</div>
-      <div className="grid ">
-        <div>Earrings</div>
-        <div>Bracelets</div>
-        <div>Necklaces</div>
-        <div>Rings</div>
-        <div>Other</div>
+      <div className="grid border border-width-2px border-black md: grid-cols-2">
+        <div className="border border-width-2px p-25 border-black">Earrings</div>
+        <div className="border border-width-2px p-25 border-black">Bracelets</div>
+        <div className="border border-width-2px p-25 border-black">Necklaces</div>
+        <div className="border border-width-2px p-25 border-black">Rings</div>
+        <div className="border border-width-2px p-25 border-black">Other</div>
       </div>
     </main>
     
