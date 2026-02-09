15b24ccf29d9523c47420c87963f37a05e8d8cef
hector1128 <hectorhoraciocordero11@gmail.com>
2026-02-09 12:50:33 -0500

    worked on globals and main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-row p-15 rounded-lg border-4 border-black">
-        <div className="basis-2/4 self-start">KJewels</div>
-        <div className="basis-1/4 self-center">Cart</div>
-        <div className="basis-1/4 object-right">Login</div>
+      <div className="flex flex-row p-14 rounded-lg border-4 border-black">
+        <button className="basis-2/4 self-">KJewels</button>
+        <button className="basis-1/4 self-center">Cart</button>
+        <button className="basis-1/4 object-right">Login</button>
       </div>
       <div>Welcome to Kjewels</div>
       <div>UCF's Finest Jewelry</div>
-      <div className="grid border border-width-2px border-black md: grid-cols-2">
-        <div className="border border-width-2px p-25 border-black">Earrings</div>
-        <div className="border border-width-2px p-25 border-black">Bracelets</div>
-        <div className="border border-width-2px p-25 border-black">Necklaces</div>
-        <div className="border border-width-2px p-25 border-black">Rings</div>
-        <div className="border border-width-2px p-25 border-black">Other</div>
+      <div className="grid border border-2 border-black md: grid-cols-2">
+        <button className="button cursor-pointer justify-self-center p-20 border-black">Earrings</button>
+        <button className="button cursor-pointer justify-self-center p-20 border-black">Bracelets</button>
+        <button className="button cursor-pointer justify-self-center p-20 border-black">Necklaces</button>
+        <button className="button cursor-pointer justify-self-center p-20 border-black">Rings</button>
+        <button className="button cursor-pointer justify-self-center p-20 border-black">Other</button>
       </div>
     </main>
-    
-
   )
 }
