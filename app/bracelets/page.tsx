156d239a7e850385cbd81cccc0f21ac1bb82fce0
hector1128 <hectorhoraciocordero11@gmail.com>
2026-02-12 00:40:37 -0500

    plan for future

--- app/bracelets/page.tsx
+++ app/bracelets/page.tsx
@@ -7,3 +7,18 @@ export default function Bracelets(){
         </main>
     )
 }
+
+
+/* 
+    PLAN
+
+    1. create nav bar
+    2. create layout for each category's page
+    3. pagination for each page
+    4. Admin and customer login
+    4. Figure out how to create/delete item from page with the proper credentials
+    5. Cart logic
+    6. Square payment integration
+    7. Design of page
+
+*/
